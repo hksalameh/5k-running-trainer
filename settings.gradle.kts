b6ee5ef0ed4602d65634fae7162bea93e6d5cf4f
hksalameh <hksalameh@gmail.com>
2026-09-03 22:10:17 +0300

    Add Gradle project settings

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Rakdatak"
+
+include(":app")
+include(":wear")
+include(":core:training")
