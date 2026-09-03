bf876d5ee8f904c677de2f1466e546c837f643c9
hksalameh <hksalameh@gmail.com>
2026-09-03 22:10:59 +0300

    Add shared training module

---
+++ core/training/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.rakdatak.core.training"
+    compileSdk = 37
+
+    defaultConfig {
+        minSdk = 28
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
